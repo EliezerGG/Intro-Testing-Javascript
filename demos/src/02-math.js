515a46ce3db74ae0eb23688b095f2353ffe849f9
Eliezer Guevara <magdiel.852@gmail.com>
2024-11-28 11:21:52 -0600

    assertions-test

---
+++ demos/src/02-math.js
@@ -0,0 +1,16 @@
+function sum(a, b) {
+  return a + b;
+}
+module.exports = sum;
+
+function multiply(a, b) {
+  return a * b;
+}
+module.exports = sum;
+
+function divide(a, b) {
+  return a / b;
+}
+module.exports = {
+  sum,multiply, divide
+};
